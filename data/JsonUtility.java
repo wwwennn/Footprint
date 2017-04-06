2f2d58f44813a1d0d5987de3d3da9ae86e07acb1
wwwennn <wenzhong@seas.upenn.edu>
2017-04-06 15:45:58 -0400

    data and html

---
+++ data/JsonUtility.java
@@ -0,0 +1,8 @@
+
+public class JsonUtility {
+	public static void main(String[] args) {
+		StringBuilder sb = new StringBuilder();
+		Scanner in = new Scanner(new File("/Users/zhongwen/Dropbox/Spring2017/594/project/data/test.json"));
+		
+	}
+}
